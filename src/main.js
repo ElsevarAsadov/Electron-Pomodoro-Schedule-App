a3db100f14edd1ed5f55fe5aea7a57eec1944ab9
ElsevarAsadov <elseverclash57@gmail.com>
2023-03-22 23:50:43 +0400

    bug_fixed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /* This is main proccess modoule which electron window is created
-here and every window events are controlled.*/
+here and every wi ndow events are controlled.*/
 
 const { app, BrowserWindow, Menu, ipcMain, dialog } = require("electron");
 const path = require("path");
@@ -20,7 +20,7 @@ class App {
       this.win.setIcon("assets/favicon.png");
       //loads html
       this._loadGui();
-      this.win.webContents.openDevTools()
+      this.win.webContents.openDevTools();
     });
   }
 
@@ -71,13 +71,9 @@ class App {
           y,
         });
 
-
-        ipcMain.on("sendMsg", (event, msg) =>{
+        ipcMain.on("sendMsg", (event, msg) => {
           this.win.webContents.send("getMsg", msg);
-        }
-        );
-
-        this.win2.webContents.openDevTools()
+        });
 
         //loading settings window gui
         this.win2.loadFile("src/settings.html");
